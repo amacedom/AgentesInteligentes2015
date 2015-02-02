33608682408105e8280ba5052fd963e7f3b7b94f
djmacky <armando_m@live.com>
2015-02-02 16:21:27 -0600

    se agregaron clases para metodos de busqueda

---
+++ src/algoritmos/DFS.java
@@ -0,0 +1,19 @@
+package algoritmos;
+
+import rompecabezas.Estado;
+
+public class DFS implements SearchMethods{
+
+	@Override
+	public Estado buscarEstado(Estado objetivo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existeEstado(Estado objetivo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
